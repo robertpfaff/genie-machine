a1c8b899415759d2b89b9d5b9eb2c2cf46df31c5
robertpfaff <42342840+robertpfaff@users.noreply.github.com>
2024-04-30 21:25:55 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,37 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="styles.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-  </head>
-<body>
-        <div class="container">
-            <div class="row row-cols-1">
-              <div class="col d-flex" id="img-fortune-teller">
-                <img src="images/fortune-teller.png" alt="fortune teller"></div>
-              <div class="col d-flex" id="instructions">
-                <pre>
-                    Would you dare to tempt fate? 
-                    Click to pick a card.
-                    Flip card to reveal your fortune.
-                </pre>
-              </div>
-              <div class="col button-div">
-                <button type="button" class="btn btn-warning" onclick="getTarotImages()" id="button" >Click to pick a card.</button>
-              </div>
-              <div class="col button-div">
-                <button type="button" class="btn btn-info" onclick=clearResult(); id="button" >Click to reset.</button>
-              </div>
-              <!-- start of flip card -->
-              <div class="flip-card">
-                <div class="flip-card-inner">
-                  <div class="flip-card-front">
-                    <img id="flip-card-id">
-                  </div>
-                  <div class="flip-card-back">
-                    <div id="fortune"></div>
-                  </div>
-                </div>
-              <!-- end of flip card -->
-              </div>
-            <!-- end of row -->
-            </div>
-          <!-- end of container -->
-          </div>
-          <script src="script.js"></script>
-        </body>
-       
+
+const getTarotImages = async () => {
+    const response = await fetch('http://127.0.0.1:3000/tarot.json', {
+        method: "GET",
+        headers: {
+            'Accept': 'application/json'
+        }
+    })
+    const tarotData = await response.json();
+    const tarotImages = tarotData.tarot
+    const tarotImageArray = tarotImages.map(card => {
+        return card;
+    })
+    postData(tarotImageArray)
+}
+const postData = (data) => {
+    console.log("Data")
+    console.log(data)
+    let tarot_index = Math.floor(Math.random() * data.length)
+    console.log("Index")
+    console.log(tarot_index)
+    let selected_image = data[tarot_index].image
+    console.log("Image")
+    console.log(selected_image)
+    let fortune = data[tarot_index].desc
+    console.log("Fortune")
+    console.log(fortune)
+    document.getElementById('flip-card-id').src = `"images/${selected_image}">`
+    document.getElementById("fortune").textContent = fortune
+}
+
+getTarotImages();
+
+function clearResult(){
+    document.getElementById('flip-card-id').src = ''
+    document.getElementById("fortune").textContent = ''
+  }
