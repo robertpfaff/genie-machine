7fd6f0f0f4cbae9703c32f63c4fbf07f19f202ef
robertpfaff <42342840+robertpfaff@users.noreply.github.com>
2024-05-02 17:56:49 -0400

    Update script.js final

--- script.js
+++ script.js
@@ -1,172 +1,39 @@
-@charset "utf-8";
-@import url('https://fonts.googleapis.com/css2?family=Allura&family=Poppins:wght@100;200;300;400;500;600;700;800&display=swap');
 
-/*  Container Settings */
-
-* {
-  display: flex;
-  box-sizing: border-box;
-  background-color: #6699cc;
-  font-family: "Poppins", sans-serif;
-  font-size: 18px;
-  justify-content: center;
-  align-items: center;
+const getTarotImages = async () => {
+    const response = await fetch('http://127.0.0.1:3000/tarot.json', {
+        method: "GET",
+        headers: {
+            'Accept': 'application/json'
+        }
+    })
+    const tarotData = await response.json();
+    const tarotImages = tarotData.tarot
+    const tarotImageArray = tarotImages.map(card => {
+        return card;
+    })
+    postData(tarotImageArray)
+}
+const postData = (data) => {
+    console.log("Data")
+    console.log(data)
+    let tarot_index = Math.floor(Math.random() * data.length)
+    console.log("Index")
+    console.log(tarot_index)
+    let selected_image = data[tarot_index].image
+    console.log("Image")
+    console.log(selected_image)
+    let fortune = data[tarot_index].desc
+    console.log("Fortune")
+    console.log(fortune)
+    document.getElementById('flip-card-id').src = `images/${selected_image}`
+    document.getElementById("fortune").textContent = fortune;
 }
 
-html, body {
-  margin: 0;
-  padding: 0;
-}  
 
-.container {
-  background-color: #6699cc;
-}
+const scroll = document.getElementById("scrollLink")
 
-.row {
-  background: #6699cc;
-  
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+function rollScroll(){
+    const scroll = document.getElementById("scrollLink")
+    scroll.scrollIntoView({ behavior: "smooth"});
 
-.col {
-  border: solid 1px #6699cc;
-}
-
-#img-fortune-teller {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-h3 {
-  display: block;
-}
-
-article {
-  width: 400px;
-}
-
-ol {
-  display: inline-block;
-  counter-reset: li;
-  list-style: none;
-  list-style: decimal;
-}
-
-ol li {
-  justify-content: start;
-  position: relative;
-  padding-left: 60px;
-  min-height: 60px;
-}
-
-ol li .active {
-  color: rgb(37, 40, 40);
-}
-
-ol li a:hover {
-  color: white;
-  text-decoration:none;
-  
-}
-
-ol li a:focus {
-  color: lightgray;
-  text-decoration:none;
-}
-
-ol li:before {
-content: counter(li);
-counter-increment: li;
-color: #fff;
-background: #f07100;
-border-radius: 50%;
-font-size: 18px;
-width: 48px;
-height: 48px;
-line-height: 48px;
-text-align: center;
-display: block;
-position: absolute;
-top: 0;
-left: 0;
-}
-
-.button-div {
-  max-width: 800px;
-  justify-content: center;
-  align-items: center;
-  padding: 10px;
-  margin: 10px;
-}
-
-#button {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-/* The flip card container - set the width and height to whatever you want. We have added the border property to demonstrate that the flip itself goes out of the box on hover (remove perspective if you don't want the 3D effect */
-.flip-card {
-  background-color: #6699cc;
-  width: 600px;
-  height: 600px;
-  perspective: 1000px; /* Remove this if you don't want the 3D effect */
-
-}
-
-#flip-card-id {
-  background-color: #6699cc;
-  width: 600px;
-  height: 600px;
-}
-
-#fortune {
-  background-color: #6699cc;
-  width: 400px;
-  height: 400px;
-  color: black;
-  font-family: 'Poppins', sans-serif;
-  font-size: 20px;
-}
-
-/* This container is needed to position the front and back side */
-.flip-card-inner {
-  position: relative;
-  width: 100%;
-  height: 100%;
-  text-align: center;
-  transition-delay: 1s;
-  transition: transform 2s;
-  transform-style: preserve-3d;
-}
-
-/* Do an horizontal flip when you move the mouse over the flip box container */
-.flip-card:hover .flip-card-inner {
-  background-color: #6699cc;
-  transition-delay: 2s;
-  transform: rotateY(180deg);
-}
-
-/* Position the front and back side */
-.flip-card-front, .flip-card-back {
-  position: absolute;
-  background-color: #6699cc;
-  width: 100%;
-  height: 100%;
-  -webkit-backface-visibility: hidden; /* Safari */
-  backface-visibility: hidden;
-}
-
-/* Style the front side (fallback if image is missing) */
-.flip-card-front {
-  background-color: #6699cc;
-  color: black;
-}
-
-/* Style the back side */
-.flip-card-back {
-  background-color: transparent;
-  transform: rotateY(180deg);
 }
